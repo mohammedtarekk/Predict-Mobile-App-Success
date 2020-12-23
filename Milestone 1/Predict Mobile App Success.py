8fc0a7fbc0b243fa0135e9ca5aeb4d82ebc89756
mohamed ahmed ahmed kamal <mohamedahmedahmedkamal@gmail.com>
2020-12-23 16:33:36 +0200

    Update Predict Mobile App Success.py
    1-adding polynimial_regression model
    2-putting each model in a function

--- Milestone 1/Predict Mobile App Success.py
+++ Milestone 1/Predict Mobile App Success.py
@@ -8,6 +8,35 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import make_column_transformer
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
+
+def polynomial_regression(degree,X_train,y_train,X_test,y_test):
+    poly_features = PolynomialFeatures(degree=degree)
+    # transforms the existing features to higher degree features.
+    X_train_poly = poly_features.fit_transform(X_train)
+    # fit the transformed features to Linear Regression
+    poly_model = linear_model.LinearRegression()
+    poly_model.fit(X_train_poly, y_train)
+    # predicting on training data-set
+    y_train_predicted = poly_model.predict(X_train_poly)
+    # predicting on test data-set
+    prediction = poly_model.predict(poly_features.fit_transform(X_test))
+    # evaluation
+    #print('Training error', metrics.mean_squared_error(y_test, y_train_predicted))
+    #print('Co-efficient of linear regression',poly_model.coef_)
+    #print('Intercept of linear regression model',poly_model.intercept_)
+    print('Mean Square Error', metrics.mean_squared_error(y_test, prediction))
+
+def linear_regression(x_train,y_train,x_test,y_test):
+    cls = linear_model.LinearRegression()
+    cls.fit(x_train, y_train)
+    # prediction
+    prediction = cls.predict(x_test)
+    # evaluation
+    #print('Co-efficient of linear regression', cls.coef_)
+    #print('Intercept of linear regression model', cls.intercept_)
+    print('Mean Square Error', metrics.mean_squared_error(np.asarray(y_test), prediction))
+
 
 # loading data
 data = pd.read_csv('AppleStore_training.csv')
@@ -39,15 +68,11 @@ top_feature = corr.index[abs(corr['user_rating'] > 0.5)]
 plt.subplots(figsize=(12, 8))
 top_corr = data[top_feature].corr()
 sns.heatmap(top_corr, annot=True)
-plt.show()
+#plt.show()
 
-# model
-cls = linear_model.LinearRegression()
-cls.fit(x_train, y_train)
+# models
 
-# prediction
-prediction = cls.predict(x_test)
+linear_regression(x_train,y_train,x_test,y_test)
+polynomial_regression(3,x_train,y_train,x_test,y_test)
 
-# evaluation
-print('Mean Square Error', metrics.mean_squared_error(np.asarray(y_test), prediction))
 
