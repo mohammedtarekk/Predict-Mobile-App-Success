4e02e0c492992bd0a06ce6fe465392f74577c5a3
mohamed ahmed ahmed kamal <mohamedahmedahmedkamal@gmail.com>
2020-12-23 20:51:07 +0200

    Update Predict Mobile App Success.py
    adding SVR and randomforest_regression models

--- Milestone 1/Predict Mobile App Success.py
+++ Milestone 1/Predict Mobile App Success.py
@@ -13,41 +13,52 @@ from sklearn.preprocessing import PolynomialFeatures
 
 def polynomial_regression(degree, X_train, y_train, X_test, y_test):
     poly_features = PolynomialFeatures(degree=degree)
-
     # transforms the existing features to higher degree features.
     X_train_poly = poly_features.fit_transform(X_train)
-
     # fit the transformed features to Linear Regression
     poly_model = linear_model.LinearRegression()
     poly_model.fit(X_train_poly, y_train)
-
     # predicting on training data-set
     y_train_predicted = poly_model.predict(X_train_poly)
-
     # predicting on test data-set
     prediction = poly_model.predict(poly_features.fit_transform(X_test))
-
     # evaluation
     # print('Training error', metrics.mean_squared_error(y_test, y_train_predicted))
     # print('Co-efficient of linear regression',poly_model.coef_)
     # print('Intercept of linear regression model',poly_model.intercept_)
-
-    print('Mean Square Error', metrics.mean_squared_error(y_test, prediction))
-
+    print('polynomial_regression: \n Mean Square Error', metrics.mean_squared_error(np.asarray(y_test), prediction))
 
 def linear_regression(x_train, y_train, x_test, y_test):
     cls = linear_model.LinearRegression()
     cls.fit(x_train, y_train)
-
     # prediction
     prediction = cls.predict(x_test)
-
     # evaluation
     # print('Co-efficient of linear regression', cls.coef_)
     # print('Intercept of linear regression model', cls.intercept_)
+    print('linear_regression: \n Mean Square Error', metrics.mean_squared_error(np.asarray(y_test), prediction))
 
-    print('Mean Square Error', metrics.mean_squared_error(np.asarray(y_test), prediction))
+from sklearn import svm
+def svr(x_train, y_train, x_test, y_test):
+    model = svm.SVR()
+    model.fit(x_train, y_train)
+    # prediction
+    prediction = model.predict(x_test)
+    # evaluation
+    # print('Co-efficient of linear regression', model.coef_)
+    # print('Intercept of linear regression model', model.intercept_)
+    print('SVR: \n Mean Square Error', metrics.mean_squared_error(np.asarray(y_test), prediction))
 
+from sklearn.ensemble import RandomForestRegressor
+def  randomforest_regression(x_train, y_train, x_test, y_test):
+    model = RandomForestRegressor()
+    model.fit(x_train, y_train)
+    # prediction
+    prediction = model.predict(x_test)
+    # evaluation
+    # print('Co-efficient of linear regression', model.coef_)
+    # print('Intercept of linear regression model', model.intercept_)
+    print('randomforest_regression: \n Mean Square Error', metrics.mean_squared_error(np.asarray(y_test), prediction))
 
 # Loading Data
 data = pd.read_csv('AppleStore_training.csv')
@@ -103,5 +114,6 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.30, shuffl
 
 # models
 linear_regression(x_train, y_train, x_test, y_test)
-# polynomial_regression(3, x_train, y_train, x_test, y_test)
-
+polynomial_regression(2, x_train, y_train, x_test, y_test)
+svr(x_train, y_train, x_test, y_test)
+randomforest_regression(x_train, y_train, x_test, y_test)
